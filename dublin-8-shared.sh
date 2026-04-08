72a54abd95ee35910ad069dc2428c04bf48bb400
Michael Kruse <llvm-project@meinersbur.de>
2026-04-08 22:40:03 +0200

    Shared and Static Libraries
    Cleanup
    
    Header
    
    Fix example
    
    Executable bit
    
    Move lib settings

---
+++ dublin-8-shared.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+set -ex
+
+# Projects build
+cmake -B build-llvm                       \
+      -S llvm                             \
+      -D LLVM_ENABLE_PROJECTS="clang;lld"
+ninja -C build-llvm
+
+# Runtimes build (static by default)
+cmake -B build-runtimes-static       \
+      -S runtimes                    \
+      -D LLVM_BINARY_DIR=build-llvm  \
+      -D LLVM_ENABLE_RUNTIMES=dublin \
+      -D DUBLIN_INCLUDE_EXAMPLES=ON  \
+      -D DUBLIN_ENABLE_STATIC=ON     \
+      -D DUBLIN_ENABLE_SHARED=OFF    \
+      -D BUILD_SHARED_LIBS=OFF
+
+# Runtimes build (shared by default)
+cmake -B build-runtimes-shared       \
+      -S runtimes                    \
+      -D LLVM_BINARY_DIR=build-llvm  \
+      -D LLVM_ENABLE_RUNTIMES=dublin \
+      -D DUBLIN_INCLUDE_EXAMPLES=ON  \
+      -D DUBLIN_ENABLE_STATIC=ON     \
+      -D DUBLIN_ENABLE_SHARED=ON     \
+      -D BUILD_SHARED_LIBS=ON        \
+
+
+cd build-runtimes-static
+ninja check-dublin
+ninja dublin-hello
+find -name "libdublin.*"
+LD_DEBUG=libs bin/dublin-hello
+ninja dublin.shared
+find -name "libdublin.*"
+cd ..
+
+cd build-runtimes-shared
+ninja check-dublin
+ninja dublin-hello
+find -name "libdublin.*"
+LD_DEBUG=libs bin/dublin-hello
+ninja dublin
+find -name "libdublin.*"
---
+++ dublin/cmake/Modules/AddDublin.cmake
@@ -0,0 +1,143 @@
+#===------------------------------------------------------------------------===#
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#===------------------------------------------------------------------------===#
+
+# Builds a library with common options of this runtime.
+#
+# Usage:
+#
+# add_dublin_library(name sources ...
+#   SHARED
+#     Build a dynamic (.so/.dll) library
+#   STATIC
+#     Build a static (.a/.lib) library
+#   OBJECT
+#     Always create an object library.
+#     Without SHARED/STATIC, build only the object library.
+# )
+function (add_dublin_library name)
+  set(options STATIC SHARED OBJECT)
+  set(multiValueArgs "")
+  cmake_parse_arguments(ARG
+    "${options}"
+    ""
+    "${multiValueArgs}"
+    ${ARGN})
+
+  set(name_static "${name}.static")
+  set(name_shared "${name}.shared")
+  set(name_object "obj.${name}")
+  if (ARG_STATIC AND NOT ARG_SHARED)
+    set(name_static "${name}")
+  elseif (NOT ARG_STATIC AND ARG_SHARED)
+    set(name_shared "${name}")
+  elseif (NOT ARG_STATIC AND NOT ARG_SHARED AND ARG_OBJECT)
+    set(name_object "${name}")
+  elseif (NOT ARG_STATIC AND NOT ARG_SHARED AND NOT ARG_OBJECT)
+    # Only one of them will actually be built.
+    set(name_static "${name}")
+    set(name_shared "${name}")
+  endif ()
+
+  if (ARG_STATIC AND ARG_SHARED)
+    set(build_static ${DUBLIN_ENABLE_STATIC})
+    set(build_shared ${DUBLIN_ENABLE_SHARED})
+  else ()
+    set(build_static ${ARG_STATIC})
+    set(build_shared ${ARG_SHARED})
+  endif ()
+  if (NOT ARG_STATIC AND NOT ARG_SHARED AND NOT ARG_OBJECT)
+    if (BUILD_SHARED_LIBS)
+      set(build_shared ON)
+    else ()
+      set(build_static ON)
+    endif ()
+  endif ()
+
+  set(build_object OFF)
+  if (ARG_OBJECT)
+    set(build_object ON)
+  elseif (build_static AND build_shared)
+    set(build_object ON)
+  endif ()
+
+  set(srctargets "") # targets that contain source files
+  set(libtargets "") # static/shared if they are built
+  set(alltargets "") # any add_library target added by this function
+  if (build_static)
+    list(APPEND srctargets "${name_static}")
+    list(APPEND libtargets "${name_static}")
+    list(APPEND alltargets "${name_static}")
+  endif ()
+  if (build_shared)
+    list(APPEND srctargets "${name_shared}")
+    list(APPEND libtargets "${name_shared}")
+    list(APPEND alltargets "${name_shared}")
+  endif ()
+  if (build_object)
+    set(srctargets "${name_object}")
+    list(APPEND alltargets "${name_object}")
+  endif ()
+
+  if (build_object)
+    add_library("${name_object}" OBJECT ${ARG_UNPARSED_ARGUMENTS})
+    set_target_properties(${name_object} PROPERTIES
+        POSITION_INDEPENDENT_CODE ON
+        FOLDER "Flang-RT/Object Libraries"
+      )
+    set(ARG_UNPARSED_ARGUMENTS "$<TARGET_OBJECTS:${name_object}>")
+  endif ()
+  if (build_static)
+    add_library("${name_static}" STATIC ${extra_args} ${ARG_UNPARSED_ARGUMENTS})
+  endif ()
+  if (build_shared)
+    add_library("${name_shared}" SHARED ${extra_args} ${ARG_UNPARSED_ARGUMENTS})
+  endif ()
+
+  if (libtargets)
+    if (BUILD_SHARED_LIBS)
+      if (build_shared)
+        set(default_target "${name_shared}")
+      else ()
+        set(default_target "${name_static}")
+      endif ()
+    else ()
+      if (build_static)
+        set(default_target "${name_static}")
+      else ()
+        set(default_target "${name_shared}")
+      endif ()
+    endif ()
+    add_library(${name}.default ALIAS "${default_target}")
+
+    if (NOT TARGET ${name})
+      add_custom_target(${name})
+      add_dependencies(${name} ${libtargets})
+    endif ()
+  endif ()
+
+  foreach (tgtname IN LISTS libtargets)
+    set_target_properties(${tgtname} PROPERTIES OUTPUT_NAME "${name}")
+  endforeach ()
+
+  foreach (tgtname IN LISTS alltargets)
+    target_include_directories(${tgtname} PUBLIC "${DUBLIN_SOURCE_DIR}/include")
+  endforeach ()
+
+  foreach (tgtname IN LISTS libtargets)
+      set_target_properties(${tgtname}
+        PROPERTIES
+          ARCHIVE_OUTPUT_DIRECTORY "${DUBLIN_OUTPUT_RESOURCE_LIB_DIR}"
+          LIBRARY_OUTPUT_DIRECTORY "${DUBLIN_OUTPUT_RESOURCE_LIB_DIR}"
+        )
+
+      install(TARGETS ${tgtname}
+          ARCHIVE DESTINATION "${DUBLIN_INSTALL_RESOURCE_LIB_PATH}"
+          LIBRARY DESTINATION "${DUBLIN_INSTALL_RESOURCE_LIB_PATH}"
+        )
+  endforeach ()
+endfunction ()
