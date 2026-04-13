d1beb3ede332706619d730afd249966d9d8c1893
Michael Kruse <meinersbur@meinersbur.de>
2026-04-13 23:58:14 +0000

    Ignore failing modes

--- dublin-15-deps.sh
+++ dublin-15-deps.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 set -ex
 
-cmake -B build-bootstrap                                          \
-      -S llvm                                                     \
-      -D LLVM_ENABLE_PROJECTS="clang;lld"                         \
-      -D LLVM_ENABLE_RUNTIMES="libunwind;libcxx;libcxxabi;dublin" \
+cmake -B build-bootstrap                                                      \
+      -S llvm                                                                 \
+      -D LLVM_ENABLE_PROJECTS="clang;lld"                                     \
+      -D LLVM_ENABLE_RUNTIMES="compiler-rt;libunwind;libcxx;libcxxabi;dublin" \
       -D DUBLIN_INCLUDE_EXAMPLES=ON
 
 cd build-bootstrap
